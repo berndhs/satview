09a466cc7a83a99c613770777355ff73e6ce988c
Bernd Stramm <bernd@pirx.(none)>
2009-11-04 20:42:39 -0500

    hope thats all

---
+++ dcomp/sjdatasrc.h
@@ -0,0 +1,47 @@
+#ifndef SJDATASRC_H
+#define SJDATASRC_H
+
+//
+//  Copyright (C) 2009 - Bernd H Stramm
+//
+#include <string.h>
+#include <stdio.h>   /* jpeglib.h needs it, but doesn't include it. */
+#include "jpeglib.h"
+#include "jerror.h"
+#ifdef __cplusplus
+#include <string>
+#include <sstream>
+#include "berndsutil.h"
+#endif
+
+#define HAVE_LIBJPEG
+#define SIZEOF(object)  ((size_t) sizeof(object))
+
+using namespace satview_jpeg;
+
+typedef struct sjdata {
+  char    * indata;
+  size_t  inlen;
+  size_t  cur_pos;
+#ifdef __cplusplus
+  std::string String () { 
+    std::string s;    
+    s = berndsutil::toString(indata)
+      + "  " + berndsutil::toString(inlen)
+      + "  " + berndsutil::toString(cur_pos) ;
+    return s;
+  }
+#endif
+    
+} sjdata;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void sj_jpeg_buf_src (j_decompress_ptr cinfo, sjdata inbuf);
+#ifdef __cplusplus
+}
+#endif
+
+#endif
