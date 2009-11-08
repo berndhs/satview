d4058aa54cc25ea861fd4489ea43f72fcd0fad04
Bernd Stramm <bernd.stramm@gmail.com>
2009-11-08 15:14:07 -0500

    default names

---
+++ satview-defaults.h
@@ -0,0 +1,23 @@
+#ifndef SATVIEW_DEFAULTS_H
+#define SATVIEW_DEFAULTS_H
+
+//
+//  Copyright (C) 2009 - Bernd H Stramm 
+//
+// This program is distributed under the terms of 
+// the GNU General Public License version 3 
+//
+// This software is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty 
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+//
+
+//
+
+#define SATVIEW_DEFAULT_PICNAME     "ECIR.JPG"
+
+#define SATVIEW_DEFAULT_S0          "localhost"
+#define SATVIEW_DEFAULT_S1          "www.bernd-stramm.com"
+#define SATVIEW_DEFAULT_S2          "192.168.1.152"
+
+#endif
